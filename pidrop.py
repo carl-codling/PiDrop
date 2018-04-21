ae5e94f05bad4f3f3eb5d7761d718ab963ab21f6
carl-codling <carlos@freefalldigital.co.uk>
2018-04-21 16:40:29 +0530

    Split large uploads in to chunks

--- pidrop.py
+++ pidrop.py
@@ -457,7 +457,11 @@ def sync_local(folder):
                 dblog('Skipping generated file:'+ name)
             elif fullname not in flist:
                 dblog('uploading : '+fullname)
-                upload(fullname)       
+                size = os.path.getsize(fullname)
+                if size < 20 * 1024 *1024:
+                    upload(fullname)    
+                else:
+                    upload_large(fullname)
             else: 
                 print('PATH EXISTS',fullname)
         # Then choose which subdirectories to traverse.
@@ -496,6 +500,40 @@ def upload(path, overwrite=False):
         return None
     dblog('uploaded as '+ res.name.encode('utf8'))
     return res
+
+
+def upload_large(path):
+    f = open(path)
+    file_size = os.path.getsize(path)
+
+    target = path[len(rootdir):]
+
+    print('UPLOAD LARGE FILE :: ',file_size ,target)
+
+    CHUNK_SIZE = 4 * 1024 * 1024
+
+    if file_size <= CHUNK_SIZE:
+
+        print(dbx.files_upload(f, target))
+
+    else:
+
+        upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
+        cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
+                                                   offset=f.tell())
+        commit = dropbox.files.CommitInfo(path=target)
+
+        while f.tell() < file_size:
+            if ((file_size - f.tell()) <= CHUNK_SIZE):
+                print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
+                                                                cursor,
+                                                                commit))
+            else:
+                dbx.files_upload_session_append(f.read(CHUNK_SIZE),
+                                                cursor.session_id,
+                                                cursor.offset)
+                print('ADDING CHUNK ')
+                cursor.offset = f.tell()
 
 
 
